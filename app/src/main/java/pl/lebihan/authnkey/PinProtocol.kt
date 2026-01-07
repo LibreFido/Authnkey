cef7b31297da08e8bdac7a83260095b9108f0465
mimi89999 <michel@lebihan.pl>
2026-01-07 18:35:44 +0100

    Fallback to basic PIN token request on INVALID_CBOR

--- app/src/main/java/pl/lebihan/authnkey/PinProtocol.kt
+++ app/src/main/java/pl/lebihan/authnkey/PinProtocol.kt
@@ -114,6 +114,7 @@ class PinProtocol(private val transport: FidoTransport) {
                     CTAP.Error.INVALID_COMMAND,
                     CTAP.Error.INVALID_PARAMETER,
                     CTAP.Error.CBOR_UNEXPECTED_TYPE,
+                    CTAP.Error.INVALID_CBOR,
                     CTAP.Error.MISSING_PARAMETER,
                     CTAP.Error.UNSUPPORTED_OPTION,
                     CTAP.Error.INVALID_SUBCOMMAND,
